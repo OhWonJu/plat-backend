e7c148f878e797bfaf31064fd1f66c9b7ef561c3
Zoody <reopard506@gmail.com>
2021-03-28 22:19:56 +0900

    1.0 User module-authentication and protect resovler

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,39 +1,43 @@
 import bcrypt from "bcrypt";
 
 import client from "../../client";
+import { portectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword }
-    ) => {
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: portectedResolver( // x(~)
+      async (
+        _,
+        { firstName, lastName, userName, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            ...(uglyPassword && { password: uglyPassword }), // ES6 문법.  ...(조건 && return Obj)
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
       }
-      const updatedUser = await client.user.update({
-        where: {
-          id: 1,
-        },
-        data: {
-          firstName,
-          lastName,
-          userName,
-          email,
-          ...(uglyPassword && { password: uglyPassword }), // ES6 문법.  ...(조건 && return Obj)
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile.",
-        };
-      }
-    },
+    ),
   },
 };
