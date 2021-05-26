25aebd595cbc8aa716f89bfb8714ecac1911fb8c
Zoody <reopard506@gmail.com>
2021-05-26 22:32:50 +0900

    fix

--- src/api/groups/placeItem/placeItem.typeDefs.js
+++ src/api/groups/placeItem/placeItem.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation{
-    placeItem(groupId: String!, itemId: String!, gird: Int!): MutationResponse!
+    placeItem(groupId: String!, itemId: String!, grid: Int!): MutationResponse!
   }
 `;
