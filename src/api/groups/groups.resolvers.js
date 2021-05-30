e424629933333a11ea0f4988b316704745d5a398
Zoody <reopard506@gmail.com>
2021-05-30 19:58:51 +0900

    'reCommit

--- src/api/groups/groups.resolvers.js
+++ src/api/groups/groups.resolvers.js
@@ -72,11 +72,6 @@ export default {
     //     });
     // },
     groups: ({ id }, _, { loggedInUser }) => {
-      console.log(id);
-      // loggedInUser context를 불러옴으로써 부분적인 protect도 가능해짐
-      // seeHashtag는 public 이지만 그 속의 groupsCount는 non-public
-
-      // pagination이 resolver가 아닌 field내에서 이루어진다.. field의 resolver화?
       return client.hashtag
         .findUnique({
           where: {
