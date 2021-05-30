c6f324bc6905c76d52c12346729e595a7f639427
Zoody <reopard506@gmail.com>
2021-05-30 17:09:21 +0900

    fix_avatar2

--- src/api/avatars/createAvatar/createAvatar.resolvers.js
+++ src/api/avatars/createAvatar/createAvatar.resolvers.js
@@ -17,32 +17,24 @@ const resolver = async (_, __, { loggedInUser }) => {
       error: "Avatar already exist.",
     };
   }
-  const avatarColor = COLOR[color];
-  if (avatarColor === undefined) {
-    return {
-      ok: false,
-      error: "Not exist color.",
-    };
-  } else {
-    await client.avatar.create({
-      data: {
-        user: {
-          connect: {
-            id: loggedInUser.id,
-          },
+  await client.avatar.create({
+    data: {
+      user: {
+        connect: {
+          id: loggedInUser.id,
         },
-        bodyUrl:
-          "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar-body.png",
-        headUrl:
-          "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar_face.png",
-        legUrl:
-          "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar_leg_right.png",
       },
-    });
-    return {
-      ok: true,
-    };
-  }
+      bodyUrl:
+        "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar-body.png",
+      headUrl:
+        "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar_face.png",
+      legUrl:
+        "https://plat-uploads.s3.ap-northeast-2.amazonaws.com/default/avatar/default_avatar_leg_right.png",
+    },
+  });
+  return {
+    ok: true,
+  };
 };
 
 export default {
