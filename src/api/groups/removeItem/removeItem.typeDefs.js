def2ea9d38fc6a53198b494e4069040d2bfccfbf
Zoody <reopard506@gmail.com>
2021-06-01 21:29:35 +0900

    removeItem

--- src/api/groups/removeItem/removeItem.typeDefs.js
+++ src/api/groups/removeItem/removeItem.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation{
-    removeItem(groupId: String!, posId: Int!): MutationResponse!
+    removeItem(groupId: String!, grid: Int!): MutationResponse!
   }
 `;
